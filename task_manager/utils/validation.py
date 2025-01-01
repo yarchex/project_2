79e1274346552628881621c7ecd4fddcdac12038
yarchex <artur-popov-06@outlook.com>
2025-01-02 01:49:03 +0300

    Update validation.py

--- task_manager/utils/validation.py
+++ task_manager/utils/validation.py
@@ -1,1 +1,11 @@
+from datetime import datetime
 
+def validate_priority(priority):
+    return priority in ["низкий", "средний", "высокий"]
+
+def validate_date(date_str):
+    try:
+        datetime.strptime(date_str, "%Y-%m-%d")
+        return True
+    except ValueError:
+        return False
