4ac297e15990c45d9af2c5a6d4ef560db8004675
yarchex <artur-popov-06@outlook.com>
2025-01-02 01:53:31 +0300

    Update decorators.py

--- task_manager/decorators.py
+++ task_manager/decorators.py
@@ -1,1 +1,11 @@
+import functools
+import logging
 
+def log_action(action):
+    def decorator(func):
+        @functools.wraps(func)
+        def wrapper(*args, **kwargs):
+            logging.info(f"{action} вызван для функции {func.__name__}")
+            return func(*args, **kwargs)
+        return wrapper
+    return decorator
