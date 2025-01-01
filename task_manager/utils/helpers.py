8a60551d352b3e29152e9ecb9a14cbebe8ed8b05
yarchex <artur-popov-06@outlook.com>
2025-01-02 01:53:53 +0300

    Update helpers.py

--- task_manager/utils/helpers.py
+++ task_manager/utils/helpers.py
@@ -1,1 +1,12 @@
+import json
 
+def save_tasks_to_file(tasks, filename="tasks.json"):
+    with open(filename, "w", encoding="utf-8") as f:
+        json.dump(tasks, f, ensure_ascii=False, indent=4)
+
+def load_tasks_from_file(filename="tasks.json"):
+    try:
+        with open(filename, "r", encoding="utf-8") as f:
+            return json.load(f)
+    except FileNotFoundError:
+        return []
